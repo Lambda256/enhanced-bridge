62d4dce9f98c58c9dc10f752f70b9b0b7cf52343
cain <gunbos1031@gmail.com>
2024-05-20 16:01:19 +0900

    feat: multisig contract manage validator logic

---
+++ contracts/multisig/MultiSig.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract MultiSig {
+    event ChangeValidatorRequest(
+        bytes32 indexed updateId,
+        uint256 changeValidatorCount
+    );
+    event ChangeValidatorSigned(
+        bytes32 indexed updateId,
+        uint256 signedCount
+    );
+    event ValidatorChanged(
+        bytes32 indexed updateId,
+        uint256 changeValidatorCount
+    );
+
+    struct Transaction {
+        address destination;
+        uint value;
+        bytes data;
+        bool executed;
+        uint256 signedCount;
+
+        mapping(address => bool) isConfirmed;
+        mapping(address => bool) possibleValidators;
+    }
+
+    struct UpdateValidator {
+        address oldValidator;
+        address newValidator;
+        uint256 threshold;
+        bool executed;
+        uint256 signedCount;
+
+        mapping(address => bool) isConfirmed;
+        mapping(address => bool) possibleValidators;
+    }
+
+    mapping(bytes32 => Transaction) public transactions;
+    mapping(bytes32 => UpdateValidator) public updateValidators;
+
+    address[] public validators;
+    mapping(address => bool) public isValidator;
+    uint256 public requiredSignatureCount;
+    uint256 public changeValidatorCount = 0;
+    bool private changeValidatorFlag = false;
+
+    constructor(
+        address[] memory _validators,
+        uint256 _threshold
+    ) {
+        require(_threshold <= _validators.length);
+        require(_threshold > (_validators.length / 2));
+
+        for (uint i = 0; i < _validators.length; i++) {
+            require(_validators[i] != address(0), "MultiSig: validator should not be the zero address");
+            isValidator[_validators[i]] = true;
+        }
+
+        validators = _validators;
+        requiredSignatureCount = _threshold;
+    }
+
+    modifier onlyValidator() {
+        require(isValidator[msg.sender], "MultiSig: caller is not a validator");
+        _;
+    }
+
+    function addValidatorRequest(
+        address validator,
+        uint256 threshold
+    ) external onlyValidator {
+        require(!changeValidatorFlag, "MultiSig: change validator request is already in progress");
+        require(validator != address(0), "MultiSig: validator should not be the zero address");
+        require(!isValidator[validator], "MultiSig: validator should not be a validator");
+        require(threshold <= validators.length + 1, "MultiSig: threshold should be less than or equal to validators.length + 1");
+        require(threshold > ((validators.length + 1) / 2), "MultiSig: threshold should be greater than (validators.length + 1) / 2");
+
+        changeValidatorFlag = true;
+
+        _updateValidator(address(0), validator, threshold);
+    }
+
+    function deleteValidatorRequest(
+        address validator,
+        uint256 threshold
+    ) external onlyValidator {
+        require(!changeValidatorFlag, "MultiSig: change validator request is already in progress");
+        require(validator != address(0), "MultiSig: validator should not be the zero address");
+        require(isValidator[validator], "MultiSig: validator should be a validator");
+        require(validators.length > 1, "MultiSig: validators.length should be greater than 1");
+        require(threshold <= validators.length - 1, "MultiSig: threshold should be less than or equal to validators.length - 1");
+        require(threshold > ((validators.length - 1) / 2), "MultiSig: threshold should be greater than (validators.length - 1) / 2");
+
+        changeValidatorFlag = true;
+
+        _updateValidator(validator, address(0), threshold);
+    }
+
+    function updateValidatorRequest(
+        address oldValidator,
+        address newValidator,
+        uint256 threshold
+    ) external onlyValidator {
+        require(!changeValidatorFlag, "MultiSig: change validator request is already in progress");
+        require(oldValidator != address(0), "MultiSig: old validator should not be the zero address");
+        require(newValidator != address(0), "MultiSig: new validator should not be the zero address");
+        require(isValidator[oldValidator], "MultiSig: old validator should be a validator");
+        require(!isValidator[newValidator], "MultiSig: new validator should not be a validator");
+        require(threshold <= validators.length, "MultiSig: threshold should be less than or equal to validators.length");
+        require(threshold > (validators.length / 2), "MultiSig: threshold should be greater than validators.length / 2");
+
+        changeValidatorFlag = true;
+
+        _updateValidator(oldValidator, newValidator, threshold);
+    }
+
+    function confirmChangeValidatorRequest(
+        bytes32 updateId
+    ) external onlyValidator {
+        require(changeValidatorFlag, "MultiSig: change validator request is not in progress");
+        UpdateValidator storage updateValidator = updateValidators[updateId];
+
+        require(!updateValidator.executed, "MultiSig: change validator request is already executed");
+        require(updateValidator.possibleValidators[msg.sender], "MultiSig: caller is not a possible validator");
+        require(!updateValidator.isConfirmed[msg.sender], "MultiSig: caller has already confirmed");
+
+        updateValidator.isConfirmed[msg.sender] = true;
+        updateValidator.signedCount++;
+
+        emit ChangeValidatorSigned(updateId, updateValidator.signedCount);
+
+        if (updateValidator.signedCount >= requiredSignatureCount) {
+            if (updateValidator.oldValidator == address(0)) {
+                _addValidator(updateValidator.newValidator);
+            } else if (updateValidator.newValidator == address(0)) {
+                _deleteValidator(updateValidator.oldValidator);
+            } else {
+                _updateValidator(updateValidator.oldValidator, updateValidator.newValidator);
+            }
+
+            changeValidatorFlag = false;
+            changeValidatorCount++;
+            requiredSignatureCount = updateValidator.threshold;
+            updateValidator.executed = true;
+
+            emit ValidatorChanged(updateId, changeValidatorCount);
+        }
+    }
+
+    function submitTransaction() external onlyValidator {}
+
+    function confirmTransaction() external onlyValidator {}
+
+    function getValidators() public view returns (address[] memory) {
+        return validators;
+    }
+
+    function getUpdateValidatorStatus(bytes32 updateId) public view returns (bool) {
+        return updateValidators[updateId].executed;
+    }
+
+    function _updateValidator(
+        address oldValidator,
+        address newValidator,
+        uint256 threshold
+    ) internal {
+        bytes32 updateId = keccak256(abi.encodePacked(oldValidator, newValidator, threshold, changeValidatorCount));
+        UpdateValidator storage updateValidator = updateValidators[updateId];
+        updateValidator.oldValidator = oldValidator;
+        updateValidator.newValidator = newValidator;
+        updateValidator.threshold = threshold;
+        for (uint i = 0; i < validators.length; i++) {
+            updateValidator.possibleValidators[validators[i]] = true;
+        }
+
+        emit ChangeValidatorRequest(updateId, changeValidatorCount);
+    }
+
+    function _addValidator(address validator) internal {
+        validators.push(validator);
+        isValidator[validator] = true;
+    }
+    function _deleteValidator(address validator) internal {
+        for (uint i = 0; i < validators.length; i++) {
+            if (validators[i] == validator) {
+                validators[i] = validators[validators.length - 1];
+                validators.pop();
+                break;
+            }
+        }
+        isValidator[validator] = false;
+    }
+    function _updateValidator(address oldValidator, address newValidator) internal {
+        for (uint i = 0; i < validators.length; i++) {
+            if (validators[i] == oldValidator) {
+                validators[i] = newValidator;
+                break;
+            }
+        }
+        isValidator[oldValidator] = false;
+        isValidator[newValidator] = true;
+    }
+}
---
+++ test/MultiSig.test.ts
@@ -0,0 +1,144 @@
+import { MultiSig } from "../typechain-types";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+
+function createUpdateId(
+  oldValidator: string,
+  newValidator: string,
+  threshold: number,
+  changeValidatorCount: number,
+) {
+  const packed = ethers.utils.solidityPack(
+    ["address", "address", "uint256", "uint256"],
+    [oldValidator, newValidator, threshold, changeValidatorCount],
+  );
+
+  return ethers.utils.keccak256(packed);
+}
+
+describe("MultiSig", () => {
+  let multiSig: MultiSig;
+  let validator1: SignerWithAddress;
+  let validator2: SignerWithAddress;
+  let validator3: SignerWithAddress;
+  let validator4: SignerWithAddress;
+
+  before(async () => {
+    const signers = await ethers.getSigners();
+    validator1 = signers[0];
+    validator2 = signers[1];
+    validator3 = signers[2];
+    validator4 = signers[3];
+
+    const MultiSig = await ethers.getContractFactory("MultiSig");
+    multiSig = await MultiSig.deploy(
+      [validator1.address, validator2.address],
+      2,
+    );
+    await multiSig.deployed();
+  });
+
+  it("should have the correct validators", async () => {
+    const validators = await multiSig.getValidators();
+    const requiredSignatures = 2;
+    const count = await multiSig.requiredSignatureCount();
+
+    expect(validators).to.eql([validator1.address, validator2.address]);
+    expect(count.toNumber()).to.equal(requiredSignatures);
+  });
+
+  it("add validator and check correct validator and required signature count", async () => {
+    const tx = await multiSig.addValidatorRequest(validator3.address, 2);
+    await tx.wait();
+
+    const updateId = createUpdateId(
+      ethers.constants.AddressZero,
+      validator3.address,
+      2,
+      0,
+    );
+
+    const confirm1 = await multiSig
+      .connect(validator1)
+      .confirmChangeValidatorRequest(updateId);
+    await confirm1.wait();
+    const updateStatusBefore =
+      await multiSig.getUpdateValidatorStatus(updateId);
+    expect(updateStatusBefore).to.equal(false);
+
+    const confirm2 = await multiSig
+      .connect(validator2)
+      .confirmChangeValidatorRequest(updateId);
+    await confirm2.wait();
+    const updateStatus = await multiSig.getUpdateValidatorStatus(updateId);
+    expect(updateStatus).to.equal(true);
+
+    const count = await multiSig.requiredSignatureCount();
+    expect(count.toNumber()).to.equal(2);
+  });
+
+  it("remove validator and check correct validator and required signature count", async () => {
+    const tx = await multiSig.deleteValidatorRequest(validator3.address, 2);
+    await tx.wait();
+
+    const updateId = createUpdateId(
+      validator3.address,
+      ethers.constants.AddressZero,
+      2,
+      1,
+    );
+
+    const confirm1 = await multiSig
+      .connect(validator3)
+      .confirmChangeValidatorRequest(updateId);
+    await confirm1.wait();
+    const updateStatusBefore =
+      await multiSig.getUpdateValidatorStatus(updateId);
+    expect(updateStatusBefore).to.equal(false);
+
+    const confirm2 = await multiSig
+      .connect(validator1)
+      .confirmChangeValidatorRequest(updateId);
+    await confirm2.wait();
+    const updateStatus = await multiSig.getUpdateValidatorStatus(updateId);
+    expect(updateStatus).to.equal(true);
+
+    const count = await multiSig.requiredSignatureCount();
+    expect(count.toNumber()).to.equal(2);
+  });
+
+  it("update validator and check correct validator and required signature count", async () => {
+    const tx = await multiSig.updateValidatorRequest(
+      validator1.address,
+      validator4.address,
+      2,
+    );
+    await tx.wait();
+
+    const updateId = createUpdateId(
+      validator1.address,
+      validator4.address,
+      2,
+      2,
+    );
+
+    const confirm1 = await multiSig
+      .connect(validator2)
+      .confirmChangeValidatorRequest(updateId);
+    await confirm1.wait();
+    const updateStatusBefore =
+      await multiSig.getUpdateValidatorStatus(updateId);
+    expect(updateStatusBefore).to.equal(false);
+
+    const confirm2 = await multiSig
+      .connect(validator1)
+      .confirmChangeValidatorRequest(updateId);
+    await confirm2.wait();
+    const updateStatus = await multiSig.getUpdateValidatorStatus(updateId);
+    expect(updateStatus).to.equal(true);
+
+    const count = await multiSig.requiredSignatureCount();
+    expect(count.toNumber()).to.equal(2);
+  });
+});
